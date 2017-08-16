3139bdc65a2dbb61c39caf571b7bb3ca8bcffd25
krishnakumare <30829563+krishnakumare@users.noreply.github.com>
2017-08-16 17:00:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,12 +8,12 @@ console.log('Loaded!');
 var img= document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
-     marginLeft = marginleft + 10;
-     img.style.marginleft = marginLeft +'px';
+     marginLeft = marginLeft + 10;
+     img.style.marginLeft = marginLeft +'px';
  }
  
  img.onclick = function()
  {
-    img.style.marginLeft ='100px';
-     //var interval = setInterval(moveRight,100);
+    //img.style.marginLeft ='100px';
+     var interval = setInterval(moveRight,100);
  };
